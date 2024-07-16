7b9a5cfb3507874404e237a5699864484ee2dcab
Rakesh Das <157940757+RakeshDas18@users.noreply.github.com>
2024-07-16 19:53:06 +0530

    Update game.js

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 let boxes=document.querySelectorAll(".box");
 let resetBtn=document.querySelector(".resetBtn");
-let msg=document.querySelector(".msg")
+let msg=document.querySelector(".msg");
 let msgContainer=document.querySelector(".msgContainer")
 let newGame=document.querySelector(".newGame")
 
